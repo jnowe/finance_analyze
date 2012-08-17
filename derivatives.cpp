9ea9b09cb2d9514aa4653af0d18a5ff49d9ff8b3
Thorsten Kurth <thorstenkurth@me.com>
2012-08-17 12:17:20 +0200

    updating project settingswhat happens?

---
+++ derivatives.cpp
@@ -0,0 +1,9 @@
+//
+//  derivatives.cpp
+//  finance_analyze
+//
+//  Created by Thorsten Kurth on 17.08.12.
+//
+//
+
+#include "derivatives.h"
---
+++ derivatives.h
@@ -0,0 +1,14 @@
+//
+//  derivatives.h
+//  finance_analyze
+//
+//  Created by Thorsten Kurth on 17.08.12.
+//
+//
+
+#ifndef __finance_analyze__derivatives__
+#define __finance_analyze__derivatives__
+
+#include <iostream>
+
+#endif /* defined(__finance_analyze__derivatives__) */
